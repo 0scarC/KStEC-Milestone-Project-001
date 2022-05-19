0c8cca3385b761d6134f85516fc74232045fac20
Oscar <admin@Admins-MacBook-Pro-2.local>
2022-05-19 16:34:34 -0500

    CSS begun, stage 01 finished

--- JS/player.js
+++ JS/player.js
@@ -1,108 +1,128 @@
-const player = document.createElement('div')
-playArea.append(player)
+// const player = document.createElement('div')
+// playArea.append(player)
 
-var pWidth = Math.floor(aWidth/20)
-var pX = Math.floor(aWidth/2)
-var pY = Math.floor(aHeight/1.25)
-var pMove = null
+// var pWidth = Math.floor(aWidth/20)
+// var pX = Math.floor(aWidth/2)
+// var pY = Math.floor(aHeight/1.25)
+// var pMove = null
 
-console.log(`Left: ${sLeft}`)
-console.log(`Right: ${sRight}`)
-console.log(`Up: ${sUp}`)
-console.log(`Down${sDown}`)
+// console.log(`Left: ${sLeft}`)
+// console.log(`Right: ${sRight}`)
+// console.log(`Up: ${sUp}`)
+// console.log(`Down: ${sDown}`)
 
 
-function pDesign(w, h, c) {
-    player.style.width = w
-    player.style.height = h
-    player.style.backgroundColor = c
-}
-pDesign(`${pWidth}px`, `${pWidth}px`, `#000`)
+// function pDesign(w, h, c) {
+//     player.style.width = w
+//     player.style.height = h
+//     player.style.backgroundColor = c
+// }
+// pDesign(`${pWidth}px`, `${pWidth}px`, `#000`)
 
-function pPosition() {
-    player.style.position = `fixed`
-    player.style.left = `${pX}px`
-    player.style.bottom = `${pY}px`
+// function pPosition() {
+//     player.style.position = `fixed`
+//     player.style.left = `${pX}px`
+//     player.style.bottom = `${pY}px`
     
-    function pDownMove() {
-        switch (pY) {
-            case sUp[0]:
-                switch (pX) {
-                    case sLeft[0] - pWidth:
-                    case sRight[0]:
-                        pY -= 1 
-                        break;
-                    default:
-                        pY = pY
-                        break;
-                }
-                break;
-            case sUp[1]:
-                switch (pX) {
-                    case sLeft[1] - pWidth:
-                        case sRight[1]:
-                        pY -= 1 
-                        break;
-                    default:
-                        pY = pY
-                        break;
-                }
-                break;
-            default:
-                pY -= 1
-                break;
-        }
-        player.style.bottom = `${pY}px`
-    }
-    setInterval(pDownMove, 1)
+//     function pDownMove() {
+//         switch (pY) {
+//             case sUp[0]:
+//                 switch (pX) {
+//                     case sLeft[0] - pWidth:
+//                     case sRight[0]:
+//                         pY -= 1 
+//                         break;
+//                     default:
+//                         pY = pY
+//                         break;
+//                 }
+//                 break;
+//             case sUp[1]:
+//                 switch (pX) {
+//                     case sLeft[1] - pWidth:
+//                         case sRight[1]:
+//                         pY -= 1 
+//                         break;
+//                     default:
+//                         pY = pY
+//                         break;
+//                 }
+//                 break;
+//             default:
+//                 pY -= 1
+//                 break;
+//         }
+//         player.style.bottom = `${pY}px`
+//     }
+//     setInterval(pDownMove, 1)
     
-    function pHorizontalMove() {
-        switch (pMove) {
-            case `right`:
-                switch (pX + pWidth) {
-                    case aRight:
-                        pX = pX
-                        break;
-                    default:
-                        pX += 1
-                        break;
-                }
-                break;
-            case `left`:
-                switch (pX) {
-                    case aLeft:
-                        pX = pX
-                        break;
-                    default:
-                        pX -= 1
-                        break;
-                }
-                break;
-        }
-        player.style.left = `${pX}px`
-    }
-    setInterval(pHorizontalMove, 1)    
+//     function pHorizontalMove() {
+//         switch (pMove) {
+//             case `right`:
+//                 switch (pX + pWidth) {
+//                     case aRight:
+//                         pX = pX
+//                         break;
+//                     case sLeft[0]:
+//                         pX = pX
+//                         break;
+//                     case sLeft[1]:
+//                         switch (pX + pWidth >= sLeft) {
+//                             case true:
+//                                 // switch (pY < sUp[1] && pY + pWidth > sDown[1]) {
+//                                 //     case true:
+//                                 //         console.log(`${pY} and ${sUp[1]}`)
+//                                 //         console.log(`${pY + pWidth} and ${sDown[1]}`)
+//                                 //         pX = pX
+//                                 //         break;
+//                                 //     default:
+//                                 //         pX += 1
+//                                 //         break;
+//                                 //     }
+//                                 // break;
+//                                 pX = pX
+//                                 break;
+//                         }
+//                     default:
+//                         pX += 1
+//                         break;
+//                 }
+//                 break;
+//             case `left`:
+//                 switch (pX) {
+//                     case aLeft:
+//                         pX = pX
+//                         break;
+//                     default:
+//                         pX -= 1
+//                         break;
+//                 }
+//                 break;
+//         }
+//         player.style.left = `${pX}px`
+//     }
+//     setInterval(pHorizontalMove, 1)    
 
-    document.addEventListener(`keydown`, function(e) {
-        switch (e.key) {
-            case `ArrowRight`:
-                pMove = `right`
-                break;
-            case `ArrowLeft`:
-                pMove = `left`
-                break;
-        }
-    })
+//     document.addEventListener(`keydown`, function(e) {
+//         switch (e.key) {
+//             case `ArrowRight`:
+//                 pMove = `right`
+//                 break;
+//             case `ArrowLeft`:
+//                 pMove = `left`
+//                 break;
+//         }
+//     })
     
-    document.addEventListener(`keyup`, function(e) {
-        switch (e.key) {
-            case `ArrowRight`:
-                pMove = null
-                break;
-            case `ArrowLeft`:
-                pMove = null
-                break;
-        }
-    })
-}
-pPosition()
+//     document.addEventListener(`keyup`, function(e) {
+//         switch (e.key) {
+//             case `ArrowRight`:
+//                 pMove = null
+//                 break;
+//             case `ArrowLeft`:
+//                 pMove = null
+//                 break;
+//         }
+//     })
+// }
+// pPosition()
--- JS/stage.js
+++ JS/stage.js
@@ -1,18 +1,29 @@
 //Stage Play Area
-const playArea = document.createElement(`div`)
+
+var playArea
 const aWidth = Math.floor(innerWidth/1.02)
 const aHeight = Math.floor(innerHeight/1.02)
-const aLeft = Math.floor(innerWidth/95)
-const aRight = Math.floor(aLeft + aWidth)
-const aBottom = Math.floor(innerHeight/95)
+const aX = Math.floor(innerWidth/95)
+const aY = Math.floor(innerHeight/95)
 
-playArea.style.width = `${aWidth}px`
-playArea.style.height = `${aHeight}px`
-playArea.style.position = `fixed`
-playArea.style.left = `${aLeft}px`
-playArea.style.bottom = `${aBottom}px`
-playArea.style.backgroundColor = `#0004`
-document.body.append(playArea)
+function stageArea(c) {
+    playArea = document.createElement(`div`)
+    document.body.append(playArea)
+
+    function aDesign() {
+        playArea.style.width = `${aWidth}px`
+        playArea.style.height = `${aHeight}px`
+        playArea.style.backgroundColor = c
+    }
+    aDesign()
+
+    function sPosition() {
+        playArea.style.position = `fixed`
+        playArea.style.left = `${aX}px`
+        playArea.style.bottom = `${aY}px`
+    }
+    sPosition()
+}
 
 //Stage Platforms
 
@@ -26,7 +37,9 @@ const sRight = []
 const sUp = []
 const sDown = []
 
-function newPlatform(w, h, c, x, y) {
+
+
+function newPlatform(w, h, c, x, y, t) {
     var platform = document.createElement(`div`)
     playArea.append(platform)
 
@@ -36,7 +49,7 @@ function newPlatform(w, h, c, x, y) {
         platform.style.width = `${sWidth}px`
         platform.style.height = `${sHeight}px`
         platform.style.backgroundColor = c
-        return sWidth, sHeight
+        platform.innerHTML = t
     }
     sDesign()
     
@@ -55,6 +68,44 @@ function newPlatform(w, h, c, x, y) {
     sDown.push(sY)
 }
 
+//Stages
 
-var platFloor = newPlatform(aWidth, aHeight/15, `#f00`, aLeft, aHeight/5)
-var plat01 = newPlatform(aWidth/2, aHeight/15, `#ff0`, aWidth/4, aHeight/2)
+const start = document.createElement(`p`).innerHTML = `START`
+const end = document.createElement(`p`).innerHTML = `END`
+
+function stage01() {
+    stageArea(`#fff`)
+    newPlatform(aWidth, aHeight/15, `#f008`, aX, aHeight/25,``)
+    var startPlat = newPlatform(aWidth/4, aHeight/15, `#ff0`, aX * 2.25, aHeight/25, `<strong>START</strong>`)
+    newPlatform(aWidth/4, aHeight/15, `#f0f`, aWidth/4 * 3 , aHeight/25, `<strong>END</strong>`)
+}
+stage01()
+
+
+function stage02() {
+    stageArea(`#f004`)
+    newPlatform(aWidth, aHeight/15, `#f00`, aX, aHeight/25)
+    newPlatform(aWidth/3, aHeight/15, `#00f`, aWidth/10, aHeight/3)
+    newPlatform(aWidth/3, aHeight/15, `#00f`, aWidth/10 * 6, aHeight/3)
+}
+
+let a = 0
+function stageTimer() {
+    switch (a) {
+        case 0:
+            a = 1
+            stage01()
+            break;
+        case 1:
+            playArea.remove()
+            a = 2
+            stage02()
+            break;
+        case 2:
+            playArea.remove()
+            a = 1
+            stage01()
+            break;
+    }
+}
+// setInterval(stageTimer, 1000)
